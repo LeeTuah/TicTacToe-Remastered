e1c6102e096eb4efcfc8f68ddb00b6537d6a5563
LeeTuah <77256247+LeeTuah@users.noreply.github.com>
2025-11-20 17:27:00 +0530

    Update main.py

--- client/main.py
+++ client/main.py
@@ -30,7 +30,24 @@ if __name__ == '__main__':
                 m_player.run()
 
             case '\'3\'':
-                pass
+                clear()
+                string = '''
++======+======+======+
+|  11  |  22  |  33  |
+|  11  |  22  |  33  |
++------+------+------+
+|  44  |  55  |  66  |
+|  44  |  55  |  66  |
++------+------+------+
+|  77  |  88  |  99  |
+|  77  |  88  |  99  |
++======+======+======+
+
+Press the respective number keys for the given squares.
+Press any key to continue...
+'''
+                print(string)
+                detect_keypress()
 
             case '\'4\'':
                 looping = False
